f85e8df3e5eccdfeab2f1961f1436040b1f63beb
Sigurd K. Würtz <201906361@post.au.dk>
2021-12-03 22:39:07 +0100

    css on match.js

--- client/src/components/Match.js
+++ client/src/components/Match.js
@@ -1,11 +1,18 @@
 const Match = (props) => {
+  const date = new Date(props.matchData.date);
+
   return (
     <div className='m-2 flex justify-evenly bg-red-400'>
       <div className='w-2/5 flex-col justify-center items-center'>
         <h1 className='text-center'>{props.matchData.team1}</h1>
         <img className='w-10 mx-auto' src={props.matchData.team1Logo} />
       </div>
-      <h1 className='w-1/5 text-center'>VERSUS</h1>
+      <div className='w-1/5 text-center'>
+        <h1 className=''>VERSUS</h1>
+        <h2 className>{date.toDateString()}</h2>
+        <h2 className=''>{props.matchData.event}</h2>
+        <a className='text-blue' href={props.matchData.link}>HLTV Link</a>
+      </div>
       <div className='w-2/5 flex-col justify-center items-center'>
         <h1 className='text-center'>{props.matchData.team2}</h1>
         <img className='w-10 mx-auto' src={props.matchData.team2Logo} />
